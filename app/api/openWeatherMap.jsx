963f42c5b85ebcb439092e02f8a0d8e979f261fa
Matteo Merzi <matteo@Matteos-MacBook-Pro.local>
2016-12-13 23:13:23 +0100

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=31c6f99e7c7639dfc40f5a7ad73494c1&units=imperial';
+
+//31c6f99e7c7639dfc40f5a7ad73494c1
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        debugger;
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
---
+++ example-arrow-functions.jsx
@@ -0,0 +1,28 @@
+// var names = ['Andrew', 'Julie', 'Jen'];
+//
+// names.forEach(function (name) {
+//   console.log('forEach', name);
+// })
+//
+// names.forEach((name) => {
+//   console.log('=>', name);
+// })
+//
+// names.forEach((name) => console.log('=>', name))
+
+
+var returnMe = (a) => a + '!'
+
+console.log(returnMe('Matteo'));
+
+
+function add (a, b){
+  return a + b;
+}
+
+var newAdd = (a, b) => a + b;
+console.log(newAdd(1, 3));
+var newAdd2 = (a, b) => {
+  return a + b;
+}
+console.log(newAdd2(1, 3));
