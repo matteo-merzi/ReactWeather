86cd7070b3c18dafd454f26843c2a3e1535f1c21
Matteo Merzi <matteo.merzi@gmail.com>
2016-12-14 22:16:55 +0100

    Test beautify

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -5,19 +5,19 @@ const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?app
 //31c6f99e7c7639dfc40f5a7ad73494c1
 
 module.exports = {
-  getTemp: function(location) {
-    var encodedLocation = encodeURIComponent(location);
-    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    getTemp: function(location) {
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-    return axios.get(requestUrl).then(function(res) {
-      if (res.data.cod && res.data.message) {
-        debugger;
-        throw new Error(res.data.message);
-      } else {
-        return res.data.main.temp;
-      }
-    }, function(res) {
-      throw new Error(res.data.message);
-    });
-  }
+        return axios.get(requestUrl).then(function(res) {
+            if (res.data.cod && res.data.message) {
+                debugger;
+                throw new Error(res.data.message);
+            } else {
+                return res.data.main.temp;
+            }
+        }, function(res) {
+            throw new Error(res.data.message);
+        });
+    }
 }
