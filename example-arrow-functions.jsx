7aef1ff6d255cd9e99848fb94a44c153fcf35f98
Matteo Merzi <matteo.merzi@gmail.com>
2016-12-14 22:47:55 +0100

    Final code format config

--- example-arrow-functions.jsx
+++ example-arrow-functions.jsx
@@ -15,12 +15,12 @@ var returnMe = (a) => a + '!'
 console.log(returnMe('Matteo'));
 
 function add(a, b) {
-  return a + b;
+    return a + b;
 }
 
 var newAdd = (a, b) => a + b;
 console.log(newAdd(1, 3));
 var newAdd2 = (a, b) => {
-  return a + b;
+    return a + b;
 }
 console.log(newAdd2(1, 3));
